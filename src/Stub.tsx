afe1ee24745077fcef412a09a12f0c464b4da0f2
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-10-31 14:51:54 +0000

    add: stub実装

---
+++ src/Stub.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Stub = () => {
+  // 複数のデータの組を格納
+  const dataSets = [
+    {
+      title: "データセット1",
+      content: "データセット1の内容です。",
+    },
+    {
+      title: "データセット2",
+      content: "データセット2の内容です。",
+    },
+    // 他のデータセットも追加できます
+  ];
+
+  // データセットを配列として返す
+  return dataSets;
+};
+
+export default Stub;

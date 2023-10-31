8341837a946ad3aa20de92af4398ef1936e0ac16
岩永千輝 <52753117+Senki-dayo@users.noreply.github.com>
2023-10-31 15:17:02 +0000

    chore: 表記変更

--- src/Stub.tsx
+++ src/Stub.tsx
@@ -1,7 +1,4 @@
-// import React from "react";
-
-const Stub = () => {
-  // 複数のデータの組を格納
+export default function Stub() {
   const dataSets = [
     {
       title: "データセット1",
@@ -11,11 +8,8 @@ const Stub = () => {
       title: "データセット2",
       content: "データセット2の内容です。",
     },
-    // 他のデータセットも追加できます
   ];
 
   // データセットを配列として返す
   return dataSets;
-};
-
-export default Stub;
+}
